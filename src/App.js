6eb3606f9ae325b5cc011495f4011012c2a7adeb
Tyler Nishida <tnishida95@gmail.com>
2019-02-23 10:27:32 -0800

    Building out components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
+import Welcome from './Welcome';
+
 class App extends Component {
+
+  state = {
+    players : [
+      { id: "0001", name: "Tyler", character: "merlin" },
+      { id: "0002", name: "Anna", character: "morgana" },
+      { id: "0003", name: "David", character: "percival" },
+      { id: "0004", name: "Cameron", character: "good1" },
+      { id: "0005", name: "Alani", character: "assassin" }
+    ]
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Welcome to Avalon!</h1>
+      <div className="root">
+        <Welcome players={this.state.players}/>
       </div>
     );
   }
