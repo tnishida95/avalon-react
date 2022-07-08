9bc60cfe2871b90cbb7d3761b5322dfeb990b8b3
tnishida95 <tnishida95@gmail.com>
2022-07-08 11:08:59 -0700

    Start fresh

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import {Typography} from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Typography>Hello, World!</Typography>
+      </header>
+    </div>
+  );
+}
+
+export default App;
